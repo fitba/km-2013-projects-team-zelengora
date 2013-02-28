5a2d3b046bc4e65d8a3a58f77db1f036908e4542
nedim-popovac <nedim.popovac@edu.fit.ba>
2013-02-28 20:18:41 +0100

    Update UserAdministration.aspx.designer.cs

--- Src/KMWeb/Administration/UserAdministration.aspx.designer.cs
+++ Src/KMWeb/Administration/UserAdministration.aspx.designer.cs
@@ -109,6 +109,6 @@ namespace KMWeb.Administration {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button txtSpremi;
+        protected global::System.Web.UI.WebControls.Button txtSpremi; 
     }
 }
