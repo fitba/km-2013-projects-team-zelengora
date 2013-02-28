75ecda41b686878a29c124043d062a2f4de704cb
Nedim Popovac <nedim.popovac@edu.fit.ba>
2013-02-28 20:42:58 +0100

    Commit sa Novim QA modulom

--- Src/KMWeb/Administration/NewRevision.aspx.designer.cs
+++ Src/KMWeb/Administration/NewRevision.aspx.designer.cs
@@ -91,6 +91,6 @@ namespace KMWeb.Administration {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnPrijedlog; 
+        protected global::System.Web.UI.WebControls.Button btnPrijedlog;
     }
 }
